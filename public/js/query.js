0e917f072500c73fd1245ae88ad96285c39bb3e2
kitakeyos <kitakeyos@gmail.com>
2022-06-02 18:54:09 +0700

    check username password is empty

--- public/js/query.js
+++ public/js/query.js
@@ -2,25 +2,23 @@
 function login() {
     var username = $("#username").val()
     var password = $("#password").val()
-    if (username === "") {
+    if (username == "") {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'Vui lòng nhập tài khoản!'
         })
-        return
-    }
-    if (password === "") {
+    } else if (password == "") {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'Vui lòng nhập mật khẩu!'
         })
-        return
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Tính năng đang được phát triển!'
+        })
     }
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Vui lòng nhập mật khẩu!'
-    })
 }
